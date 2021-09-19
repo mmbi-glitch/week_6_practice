75ef9c0ebf5b7522fb2318e379ae7a5a0d137983
mmbillah <mmbillah02@gmail.com>
2021-09-19 12:01:10 +1200

    i added new lines

--- ex-4-null-ptr.cpp
+++ ex-4-null-ptr.cpp
@@ -14,4 +14,8 @@ Important points about this program:
   • note that there is no ordinary variable in the program
   • we load the value NULL into the pointer
   • this is a BAD program – do not display the data of the NULL pointer, it doesnt even work
+  • programming style - all the following are valid:
+      (1) int* ptr
+      (2) int * ptr
+      (3) int *ptr < this is the best, tho
 */
